7d512671ce3892079349f4fef95f899999200e3c
Jesse <jesse_11biscuit@hotmail.com>
2022-11-19 14:50:59 +1030

    major update to index and template js

---
+++ index.js
@@ -0,0 +1,129 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const createHTML = require('./src/HTML-template');
+
+const employees = [];
+
+worker = async () => {
+    const workerAnswers = await inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your team id number?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: 'What is your Role?',
+        choices: ['Manager', 'Engineer', 'Intern'],
+    },
+]);
+
+if(workerAnswers.role === 'Intern') {
+    const internAnswer = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school do you attend?',
+        },
+    ])
+    const internCardInfo = new Intern(
+        workerAnswers.name,
+        workerAnswers.id,
+        workerAnswers.email,
+        internAnswer.school
+    )
+    console.log(internCardInfo);
+    employees.push(internCardInfo);
+    addMember();
+}
+
+else if(workerAnswers.role === 'Engineer') {
+    const engineerAnswer = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your github username?',
+        },
+    ])
+    const engineerCardInfo = new Engineer(
+        workerAnswers.name,
+        workerAnswers.id,
+        workerAnswers.email,
+        engineerAnswer.github
+    )
+    console.log(engineerCardInfo);
+    employees.push(engineerCardInfo);
+    addMember();
+}
+
+else if (workerAnswers.role === 'Manager') {
+    const managerAnswer = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'officenumber',
+            message: 'What is your office number?',
+        },
+    ])
+    const managerCardInfo = new Manager(
+        workerAnswers.name,
+        workerAnswers.id,
+        workerAnswers.email,
+        managerAnswer.officenumber
+    )
+    console.log(managerCardInfo);
+    employees.push(managerCardInfo);
+    addMember();
+}};
+
+addMember = async () => {
+    const anotherMember = await inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'team',
+            message: 'Would you like to add another team member?'
+        }])
+        
+        if(anotherMember.team == true) {
+            worker()
+        } 
+        else {
+            console.log(employees)
+            return employees;
+        }
+};
+
+
+function writeToFile(fileName, data) {
+    fs.writeFile('./Team.html', data, err => {
+        if (err) throw err;
+        else {
+        console.log('Success!');
+        }
+    })};
+
+
+worker()    
+    .then(employees)
+    .then(employees => {
+        return createHTML(employees);
+    })
+    .then(writehtml => {
+        return writeToFile(writehtml);
+    })
+    .catch(err => {
+     console.log(err);
+    });
---
+++ src/HTML-template.js
@@ -0,0 +1,108 @@
+
+createHTML = (data) => {
+    console.log(data);
+    htmlArray = []; 
+
+    for (let i = 0; i < data.length; i++) {
+        const eachEmployee = data[i];
+        const employeeRole = employee.getRole(); 
+
+        if (employeeRole === 'Manager') {
+            const managerCard = createManager(eachEmployee);
+            htmlArray.push(managerCard);
+        }
+        if (employeeRole === 'Engineer') {
+            const engineerCard = createEngineer(eachEmployee);
+            htmlArray.push(engineerCard);
+        }
+        if (employeeRole === 'Intern') {
+            const internCard = createIntern(eachEmployee);
+            htmlArray.push(internCard);
+        }
+    };
+
+    const cards = htmlArray.join('');
+    const generateallCards = pageTemplate(cards); 
+    return generateallCards;
+};
+
+
+const pageTemplate = function(cards) {   
+    return`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="./dist/style.css">
+    </head>
+    <body>
+        <header class="container">
+            <div class="jumbotron text-center">
+                <nav class="container">
+                    <h1>Team Profile</h1>
+                </nav>
+            </div>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row">
+                    ${cards}
+                </div>
+            </div>
+        </main>
+        
+    </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
+    </html>
+  `
+};
+
+const createManager = function(manager) {
+    return `
+    <div class="col-4">
+        <div class="card mx-auto mb-3">
+        <h4 class="card-header">${manager.name}<br/><br/>Manager</h4>
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${manager.id}</li>
+            <li class="list-group-item">Email Address: ${manager.email}</li>
+            <li class="list-group-item">Phone Number: ${manager.officenumber}</li>
+        </ul>
+    </div>
+</div>`
+};
+
+const createEngineer = function(engineer) {
+    return `
+    <div class="col-4">
+        <div class="card mx-auto mb-3">
+        <h4 class="card-header">${engineer.name}<br/><br/>Engineer</h4>
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engineer.id}</li>
+            <li class="list-group-item">Email Address: ${engineer.email}</li>
+            <li class="list-group-item">GitHub: https://github.com/${engineer.github}</li>
+        </ul>
+    </div>
+</div> `
+};
+
+const createIntern = function(intern) {
+    return `
+    <div class="col-4">
+        <div class="card mx-auto mb-3">
+        <h4 class="card-header">${intern.name}<br/><br/>Intern</h4>
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${intern.id}</li>
+            <li class="list-group-item">Email Address: ${intern.email}</li>
+            <li class="list-group-item">School: ${intern.school}</li>
+        </ul>
+    </div>
+</div>`
+};
+
+
+
+module.exports = createHTML;
